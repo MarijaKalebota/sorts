f6e3c71d4b9f1252f10a57927ffcca5fefd40f52
Marija Kalebota Kodzoman <marija.kalebota.kodzoman@gmail.com>
2020-04-13 18:41:04 -0700

    Implement Bubble Sort. Define some tests.

---
+++ bubble-sort/entry-point.py
@@ -0,0 +1,15 @@
+def bubble_sort(input_array):
+    # assumes input is list
+    for i in range(1, len(input_array)):
+        for j in range(len(input_array)-i):
+            if input_array[j+1] < input_array[j]:
+                temp = input_array[j]
+                input_array[j] = input_array[j+1]
+                input_array[j+1] = temp
+    return input_array
+
+
+
+if __name__ == "__main__":
+    INPUT_ARRAY = [17, 2, 9, 0, 0, -5, 17, 8, 1]
+    print(bubble_sort(INPUT_ARRAY))
---
+++ bubble-sort/tests/unit/test_bubble_sort.py
@@ -0,0 +1,35 @@
+def test_input_not_array():
+    assert False
+
+
+def test_empty_input():
+    assert False
+
+
+def test_input_one_element():
+    assert False
+
+
+def test_numbers():
+    assert False
+
+
+def test_letters():
+    assert False
+
+
+def test_negative_numbers():
+    assert False
+
+
+def test_all_equal():
+    assert False
+
+
+def test_mixed_types_in_input():
+    assert False
+
+
+def test_non_comparable_types():
+    assert False
+
