302808bb58f091a989180d4dffd4c64db064ff6c
Marija Kalebota Kodzoman <marija.kalebota.kodzoman@gmail.com>
2020-04-14 12:14:42 -0700

    Set up tests to use unittest module. Rename entry-point to entry_point.

--- bubble-sort/tests/unit/test_bubble_sort.py
+++ bubble-sort/tests/unit/test_bubble_sort.py
@@ -1,34 +1,37 @@
-def test_input_not_array():
-    assert False
+import unittest
+
+from ..entry_point import bubble_sort
 
 
-def test_empty_input():
-    assert False
+class TestBubbleSort(unittest.TestCase):
+
+    def test_input_not_array(self):
+        assert False
+
+    def test_empty_input(self):
+        assert False
+
+    def test_input_one_element(self):
+        assert False
+
+    def test_numbers(self):
+        assert False
+
+    def test_letters(self):
+        assert False
+
+    def test_negative_numbers(self):
+        assert False
+
+    def test_all_equal(self):
+        assert False
+
+    def test_mixed_types_in_input(self):
+        assert False
+
+    def test_non_comparable_types(self):
+        assert False
 
 
-def test_input_one_element():
-    assert False
-
-
-def test_numbers():
-    assert False
-
-
-def test_letters():
-    assert False
-
-
-def test_negative_numbers():
-    assert False
-
-
-def test_all_equal():
-    assert False
-
-
-def test_mixed_types_in_input():
-    assert False
-
-
-def test_non_comparable_types():
-    assert False
+if __name__ == '__main__':
+    unittest.main()
